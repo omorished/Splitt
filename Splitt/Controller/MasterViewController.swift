8405979a3804c440a0081b6e2074c3ace84f797d
osamah morished <omorished@gmail.com>
2019-06-09 06:50:09 +0300

    complete table view design and creat dummy data

---
+++ Splitt/Controller/MasterViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MasterViewController.swift
+//  Splitt
+//
+//  Created by Os! on 09/06/2019.
+//  Copyright © 2019 Os!. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [Any]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        }
+
+
+    // MARK: - Segues
+
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "showDetail" {
+//            if let indexPath = tableView.indexPathForSelectedRow {
+//                let object = objects[indexPath.row] as! NSDate
+//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//                controller.detailItem = object
+//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+//            }
+//        }
+//    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel!.text = names[indexPath.row]
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
+    }
+
+}
+
---
+++ Splitt/Data-Source/Images+Colors+Names.swift
@@ -0,0 +1,24 @@
+//
+//  Images+Colors+Names.swift
+//  Splitt
+//
+//  Created by Os! on 09/06/2019.
+//  Copyright © 2019 Os!. All rights reserved.
+//
+
+import UIKit
+
+//images
+let camera = UIImage(named: "1")
+let city = UIImage(named: "2")
+let animal = UIImage(named: "3")
+let bouqet = UIImage(named: "4")
+let stands = UIImage(named: "5")
+let urben = UIImage(named: "6")
+
+
+//names
+let names = ["CAMERA", "CITY", "ANIMAL", "BOUQET", "STAND", "URBEN"]
+
+//colors
+let colors = [#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
